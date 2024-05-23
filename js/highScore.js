58db5eb0759b92133d49a39394db42da86b37199
nshahaf <39031036+nshahaf@users.noreply.github.com>
2024-05-23 22:54:12 +0300

    high score table now shows best of each level

--- js/highScore.js
+++ js/highScore.js
@@ -4,6 +4,47 @@ var highScoreTable
 retriveFromLocalStorage()
 updateHighScoreTable()
 
+function retriveFromLocalStorage() {
+    highScoreTable = JSON.parse(localStorage.getItem("highScore"))
+    // console.log('highScoreTable:', highScoreTable)
+    if (highScoreTable === null) {
+        highScoreTable = []
+        return
+    }
+
+    // highScoreTable = highScoreTable.sort((score1, score2) => { return score1.time - score2.time })
+}
+
+function updateHighScoreTable() {
+    if (highScoreTable === null) return
+    var length = highScoreTable.length
+    if (length === 0) return
+    highScoreTable.sort()
+    var easyHighScore = []
+    var hardHighScore = []
+    var extremeHighScore = []
+
+    for (var i = 0; i < length; i++) {
+        if (highScoreTable[i].level === 'easy') easyHighScore.push(highScoreTable[i])
+        else if (highScoreTable[i].level === 'hard') hardHighScore.push(highScoreTable[i])
+        else if (highScoreTable[i].level === 'extreme') extremeHighScore.push(highScoreTable[i])
+    }
+
+
+    //constract new highScore table with best from each level
+    highScoreTable = []
+    if (easyHighScore.length > 0) highScoreTable.push(easyHighScore[0])
+    if (hardHighScore.length > 0) highScoreTable.push(hardHighScore[0])
+    if (extremeHighScore.length > 0) highScoreTable.push(extremeHighScore[0])
+
+    //update DOM
+    for (var i = 0; i < highScoreTable.length; i++) {
+        document.getElementById(`name${i + 1}`).innerText = highScoreTable[i].name
+        document.getElementById(`level${i + 1}`).innerText = highScoreTable[i].level
+        document.getElementById(`time${i + 1}`).innerText = highScoreTable[i].time
+    }
+}
+
 function saveToLocalStorage() {
     var level
     if (gameDiff === 0) level = 'easy'
@@ -20,29 +61,6 @@ function saveToLocalStorage() {
     highScoreTable = highScoreTable.sort((score1, score2) => { return score1.time - score2.time })
     localStorage.setItem('highScore', JSON.stringify(highScoreTable))
 
-}
-
-function retriveFromLocalStorage() {
-    highScoreTable = JSON.parse(localStorage.getItem("highScore"))
-    // console.log('highScoreTable:', highScoreTable)
-    if (highScoreTable === null) {
-        highScoreTable = []
-        return
-    }
-    // highScoreTable = highScoreTable.sort((score1, score2) => { return score1.time - score2.time })
-}
-
-function updateHighScoreTable() {
-    if (highScoreTable === null) return
-    var length = highScoreTable.length
-    if (length === 0) return
-    if (length > 3) length = 3
-
-    for (var i = 0; i < length; i++) {
-        document.getElementById(`name${i+1}`).innerText = highScoreTable[i].name
-        document.getElementById(`level${i+1}`).innerText = highScoreTable[i].level
-        document.getElementById(`time${i+1}`).innerText = highScoreTable[i].time
-    }
 }
 
 function makeId(length = 6) {
